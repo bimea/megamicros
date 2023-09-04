e2dbfe2ae08f6f1460a1e77a0367582c643236f1
Bruno Gas <bruno.gas@biimea.com>
2023-09-04 17:43:48 +0200

    update

--- src/megamicros/core/base.py
+++ src/megamicros/core/base.py
@@ -36,6 +36,15 @@ class MemsArray:
     """ MEMs array base class.
 
     The MemsArray class models the operation of an antenna composed of any number of microphones
+    Les microphones de l'antenne sont obligatoireement numérotés de 0 à `available_mems_number`. 
+    Certains peuvent ne pas être *actifs*. `mems_number` donne le nombre de microphnes actifs. 
+    Les microphones sont définis par une position relative au centre de l'antenne. 
+    Ces positions peuvent ne pas être connues. Il n'est alors pas possible de faire du filtrage spatial.
+
+    Antenna microphones must be numbered from 0 to `available_mems_number`. 
+    Some may not be *active*. `mems_number` gives the number of active microphones. 
+    Microphones are defined by a position relative to the center of the antenna.
+    Tese positions may not be known. In this case, spatial filtering is not possible.
 
     Attributes
     ----------
@@ -59,20 +68,57 @@ class MemsArray:
     __available_mems: tuple
     __analogs: tuple
     __available_analogs: tuple
-    __mems_position: np.ndarray
+    __mems_position: np.ndarray | None
 
     @property
     def mems_number( self ) -> int:
-        """ Get the MEMs number """
-        return len( self.__mems_position )
+        """ Get the active MEMs number """
+        return len( self.__mems )
     
     @property
-    def mems_position( self ) -> np.ndarray:
+    def available_mems_number( self ) -> int:
+        """ Get the available MEMs number """
+        return len( self.__available_mems )
+
+    @property
+    def analogs_number( self ) -> int:
+        """ Get the active analogs number """
+        return len( self.__analogs )
+    
+    @property
+    def available_analogs_number( self ) -> int:
+        """ Get the available analogs number """
+        return len( self.__available_analogs )
+    
+    @property
+    def mems_position( self ) -> np.ndarray | None:
         """ Get the antenna mems positions
         
         Returns
         -------
-            mems_position : np.ndarray
+            mems_position : np.ndarray | None
                 array of 3D MEMs positions  
         """
         return self.__mems_position
+    
+    def __init__( self, available_mems_number:int|None=None, mems_position:np.ndarray|None=None ):
+        """Create an antenna object
+
+        One of the two MEMs parameters should be given. 
+
+        Parameters:
+        -----------
+        available_mems_number : int | None
+            The total number of MEMs composing the antenna
+        mems_position = np.ndarray | None
+            The positions of the MEMs relative to the center of the antenna
+        """
+
+        if available_mems_number is None and mems_position is None:
+            raise Exception( f"At least one of the two parameters `available_mems_number` or `mems_position` should be given" )
+        elif mems_position is None:
+            self.__mems_position = None
+            self.__available_mems = [i for i in available_mems_number]
+        else:
+            self.__mems_position = mems_position
+            self.__available_mems = [i for i in len( mems_position )]
