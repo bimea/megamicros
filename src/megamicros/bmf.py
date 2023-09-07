73c1786f645a47043c1a40b00e6540a39d821449
Gas Bruno <bruno.gas@sorbonne-universite.fr>
2023-09-07 22:23:57 +0200

    update

--- src/megamicros/bmf.py
+++ src/megamicros/bmf.py
@@ -62,16 +62,16 @@ class Beamformer:
     """ Base class for beamformers"""
 
     # bmf properties
-    __mems_position: np.ndarray         = None  # 3D MEMs positions from the antenna center
-    __space_quantization: np.ndarray    = None  # locations number per meters (space frequency)
-    __space_size: np.ndarray            = None
-    __sampling_frequency: float         = None
-    __fft_win_size: int                 = None
+    __mems_position: np.ndarray | None          = None  # 3D MEMs positions from the antenna center
+    __space_quantization: np.ndarray | None     = None  # locations number per meters (space frequency)
+    __space_size: np.ndarray | None             = None
+    __sampling_frequency: float | None          = None
+    __fft_win_size: int | None                  = None
 
     # for internal purpose
-    __freq_number: int                  # spectral bands number
-    __loc_number: int                   # Total space locations number
-
+    __freq_number: int                                  # spectral bands number
+    __loc_number: int                                   # Total space locations number
+    __locations: np.ndarray | None              = None
 
     def setMemsPosition( self, mems_position: np.ndarray ) -> None:
 
@@ -111,6 +111,13 @@ class Beamformer:
 
 
     def setSpaceSize( self, ss: np.ndarray|tuple ) -> None:
+        """ Set the beamformer working space dimensions 
+        
+        Note that the working space has nothing to do with the room space. 
+        The working space is the area where the beamformer perfoms the source localization.
+        Its definition is relative to the antenna center. 
+        Use the `Beamformer.moveAntenna()` method to stop centering of the working space.
+        """
         
         if type( ss ) is tuple or type( ss ) is list:
             if len( ss ) != 3:
@@ -125,6 +132,34 @@ class Beamformer:
         log.info( f' .Set beamformer space size to {ss} meters' )
         
         
+    def getLocations( self ) -> np.ndarray:
+        """ Get the locations matrix
+        
+        The locations matrix is a 3D array of 3D points coverring the beamformer working space
+        """
+
+        if self.__locations is None:
+            raise MuBmfException( f"No locations found. The beamformer seems not initialized. Please use the `Beamformer.init()` method before." )
+
+        return self.__locations 
+
+
+    def getLocationsNumber( self ) -> tuple:
+        """ Get the locations number among x, y and z axis
+        
+        The locations matrix is a 3D array of 3D points coverring the beamformer working space
+        """
+
+        if self.__locations is None:
+            raise MuBmfException( f"No locations found. The beamformer seems not initialized. Please use the `Beamformer.init()` method before." )
+
+        # locations number
+        loc_number_x = int( self.__space_size[0,0] * self.__space_quantization[0,0] )
+        loc_number_y = int( self.__space_size[0,1] * self.__space_quantization[0,1] )
+        loc_number_z = int( self.__space_size[0,2] * self.__space_quantization[0,2] )
+
+        return [loc_number_x, loc_number_y, loc_number_z] 
+
 
     def __init__( self, mems_position: np.ndarray|None=None, sampling_frequency: float|None=None, window_size:int|None=None, space_quantization:float|None=None, space_size:float|None=None ):
 
@@ -214,19 +249,19 @@ class Beamformer:
         log.info( f"  > Space quantum size is ({dx:.2f} x {dy:.2f} x {dz:.2f}) meters")
 
         # Build the locations 3D (centered) coordinates
-        locations = np.ndarray( ( loc_number_x*loc_number_y*loc_number_z, 3 ) )
+        self.__locations = np.ndarray( ( loc_number_x*loc_number_y*loc_number_z, 3 ) )
         for x in range( loc_number_x ):
             for y in range( loc_number_y):
                 for z in range( loc_number_z):
                     i = x * loc_number_y * loc_number_z + y * loc_number_z + z
-                    locations[i] = np.array( [x*dx+dx/2-dimx/2, y*dy+dy/2-dimy/2, z*dz+dz/2-dimz/2] )
+                    self.__locations[i] = np.array( [x*dx+dx/2-dimx/2, y*dy+dy/2-dimy/2, z*dz+dz/2-dimz/2] )
         
         # Init distance matrix
         log.info( f" .Build distances matrix D ({self.__loc_number} x {mems_number})" ) 
         self._D = np.ndarray( (self.__loc_number, mems_number), dtype=float )
         for s in range( self.__loc_number ):
             for m in range( mems_number ):
-                self._D[s, m] = np.linalg.norm( np.array( self.__mems_position[m] ) - locations[s] )
+                self._D[s, m] = np.linalg.norm( np.array( self.__mems_position[m] ) - self.__locations[s] )
 
         # Allocate and build the H complex transfer function matrix (preformed channels)
         log.info( f" .Build preformed channels matrix H ({self.__freq_number} x {self.__loc_number} x {mems_number})" ) 
