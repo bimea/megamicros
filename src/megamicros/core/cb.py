b3991368935fb8c44a990241ce52adef9ccc089f
Bruno Gas <bruno.gas@bimea.io>
2024-02-26 00:03:22 +0100

    update

--- src/megamicros/core/cb.py
+++ src/megamicros/core/cb.py
@@ -1,4 +1,4 @@
-# megamicros.core.np.py base class for antenna connected to a numpy array input stream
+# megamicros.core.cb.py base class for antenna connected to external callback function
 #
 # Copyright (c) 2023 Sorbonne Université
 # Author: bruno.gas@sorbonne-universite.fr
@@ -22,7 +22,7 @@
 # THE SOFTWARE.
 
 
-""" Provide the class for antenna with MEMs signals extracted from a numpy array 
+""" Provide the class for antenna with MEMs signals extracted by an external callback function 
 
 Documentation
 -------------
@@ -44,36 +44,41 @@ DB_PROCESSING_DELAY_RATE				= 2/10						# computing delay rate relative to trans
 # Exception dedicaced to Megamicros NP systems
 # =============================================================================
 
-class MuNPException( MuException ):
-    """Exception base class for Megamicros NP systems """
+class MuCBException( MuException ):
+    """Exception base class for Megamicros CB systems """
 
 
 # =============================================================================
-# The MemsArrayNP base class
+# The MemsArrayCB base class
 # =============================================================================
 
-class MemsArrayNP( MemsArray ):
-    """ MEMs array class with input stream connected to a numpy array given as input.
+class MemsArrayCB( MemsArray ):
+    """ MEMs array class with input stream connected to an external callback function as input.
 
-    The MemsArrayNP class is a base class for antenna with MEMs signals extracted from a numpy array.
-    We do not have any informations about the antenna that was used to record the data. So we have to set the antenna settings manually before using data.
-    A MemsArrayNp object is supposed to work well with numpy array of int32 or float32 data type under the condition that settings are in correpondance with the data.
+    As for NP antenna, we do not have any informations about the antenna that is used to play the data. 
+    So we have to set the antenna settings manually before running it.
+    A MemsArrayCB object is supposed to work well with callback which deliver binary data bint32 or bfloat32 or nupy array data int32 or float32.
 
-    Counter and status channels are not supposed to be in the input data. Those channels will be added to the output stream is the user requests them.
+    Counter and status channels can be present in the input data.
     """
 
     __input: np.ndarray = None
     __input_index: int = 0
     __input_dtype: np.dtype = None
+    __user_in_callback = None
+    __user_in_callback_args = None
+    __input_dtype: np.dtype = None
 
 
-    def __init__( self, input: np.ndarray, **kwargs ) -> None :
-        """ Connect the antenna input stream to a numpy input array
+    def __init__( self, user_in_callback, user_in_callback_args, **kwargs ) -> None :
+        """ Connect the antenna input stream to the user callback
         
         Parameters
         ----------
-        input: np.ndarray
-            input numpy array
+        user_in_callback: callback
+            The user callback function that will deliver the input data
+        user_in_callback_args: array
+            The arguments that will be provided to the user callback function
         """
 
         # Init base class
@@ -83,36 +88,37 @@ class MemsArrayNP( MemsArray ):
         if len( kwargs ) > 0:
             self._set_settings( [], kwargs )
 
-        self.__input = input
-        self.__input_index = 0
-        self.__input_dtype = input.dtype
+        self.__user_in_callback = user_in_callback
+        self.__user_in_callback_args = user_in_callback_args
 
-        log.info( f" .MemsArrayNP object created with input array {input.shape[0]} data x {input.shape[1]} channels" )
+        log.info( f" .MemsArrayCB object created" )
 
-    def setAvailableMems( self, available_mems: int|tuple|list|np.ndarray ) -> None :
-        """ Overload the parent `setAvailableMems()` method for control with __input data
+
+    def setInCallback( self, callback: any ) -> None :
+        """ Set the user callback function 
+        
+        Parameters
+        ----------
+        callback: any
+            The user callback function that will deliver the input data
         """
 
-        super().setAvailableMems( available_mems )
-
-        channels_number = self.available_mems_number + self.available_analogs_number
-        if channels_number > self.__input.shape[1]:
-            raise MuNPException( f"Available channels number ({channels_number}) is greater than the number of channels in the input array ({self.__input.shape[1]})" )
+        self.__user_in_callback = callback
 
 
-    def setAvailableAnalogs( self, available_analogs: int|tuple|list|np.ndarray ) -> None :
-        """ Overload the parent `setAvailableAnalogs()` method for control with __input data
+    def setInCallbackArgs( self, args: any ) -> None :
+        """ Set the user callback arguments 
+        
+        Parameters
+        ----------
+        args: any
+            The arguments that will be provided to the user callback function
         """
 
-        super().setAvailableAnalogs( available_analogs )
-
-        channels_number = self.available_mems_number + self.available_analogs_number
-        if channels_number > self.__input.shape[1]:
-            raise MuNPException( f"Available channels number ({channels_number}) is greater than the number of channels in the input array ({self.__input.shape[1]})" )
+        self.__user_in_callback_args = args
 
 
-
-    def _set_settings( self, args, kwargs ) -> None :
+    def _set_settings( self, args, kwargs: dict ) -> None :
         """ Set settings for MemsArrayNP objects 
         
         Parameters
@@ -125,20 +131,20 @@ class MemsArrayNP( MemsArray ):
 
         # Check direct args
         if len( args ) > 0:
-            raise MuNPException( "Direct arguments are not accepted for run() method" )
+            raise MuCBException( "Direct arguments are not accepted for run() method" )
         
         try:  
-            log.info( f" .Install MemsArrayNP settings" )
+            log.info( f" .Install MemsArrayCB settings" )
 
             # Set settings
             keys_to_remove = []
             for key, value in kwargs.items():   
-                if key == "available_mems_number":
-                    self.setAvailableMems( value )
+                if key == "user_in_callback":
+                    self.setInCallback( value )
                     keys_to_remove.append( key )
 
-                elif key == "available_analogs_number":
-                    self.setAvailableAnalogs( value )
+                elif key == "user_in_callback_args":
+                    self.setInCallbackArgs( value )
                     keys_to_remove.append( key )
             
             if len( keys_to_remove ) > 0:
@@ -147,36 +153,34 @@ class MemsArrayNP( MemsArray ):
 
             # Set parent settings
             super()._set_settings( args, kwargs )
+
                     
         except Exception as e:
-            raise MuNPException( f"Settings install failed: {e}")
+            raise MuCBException( f"Settings install failed: {e}")
 
 
     def _check_settings( self ) -> None :
-        """ Check settings values for MemsArrayNP """
+        """ Check settings values for MemsArrayCB """
 
-        # We cannot call the parent check_settings() method as it is not compatible for DB
         log.info( f" .Pre-execution checks for MemsArray.run()" )
         super()._check_settings()
 
         # Here we are
-        log.info( f" .Pre-execution checks for MemsArrayNP.run()" )
+        log.info( f" .Pre-execution checks for MemsArrayCB.run()" )
 
         # We have to control the input array shape with the settings
-        if self.__input is None:
-            raise MuNPException( f"No input array as stream for MemsArrayNP object" )
+        if self.__user_in_callback is None:
+            raise MuCBException( f"No input callback given for MemsArrayCB object" )
 
-        channels_number = self.available_mems_number + self.available_analogs_number
-        if channels_number > self.__input.shape[1]:
-            raise MuNPException( f"Available channels number ({channels_number}) is greater than the number of channels in the input array ({self.__input.shape[1]})" )
-
-
+        if self.__user_in_callback_args is None:
+            raise MuCBException( f"No input callback arguments given for MemsArrayCB object" )
+        
 
     def run( self, *args, **kwargs ) :
         """ The main run method that run over the input data """
 
         if len( args ) > 0:
-            raise MuNPException( f"Run() method does not accept direct arguments" )
+            raise MuCBException( f"Run() method does not accept direct arguments" )
         
         log.info( f" .Starting run execution" )
                 
@@ -185,14 +189,14 @@ class MemsArrayNP( MemsArray ):
             self._set_settings( [], kwargs=kwargs )
 
         except Exception as e:
-            raise MuNPException( f"Cannot run: settings loading failed ({type(e).__name__}): {e}" )
+            raise MuCBException( f"Cannot run: settings loading failed ({type(e).__name__}): {e}" )
         
         # Check settings values
         try:
             self._check_settings()
 
         except Exception as e:
-            raise MuNPException( f"Unable to execute run: control failure  ({type(e).__name__}): {e}" )
+            raise MuCBException( f"Unable to execute run: control failure  ({type(e).__name__}): {e}" )
 
         # verbose
         if self.duration == 0:
@@ -219,7 +223,7 @@ class MemsArrayNP( MemsArray ):
         
 
     def __run_thread( self ):
-        """ Run thread for the MemsArrayNP object """
+        """ Run thread for the MemsArrayCB object """
 
         # Set the mask transfert
         mask = list( np.isin( self.available_mems, self.mems ) )
@@ -229,6 +233,19 @@ class MemsArrayNP( MemsArray ):
         # Add analog channels if any
         if self.available_analogs_number > 0 and self.analogs_number > 0:
             mask = mask + list( np.isin( self.available_analogs, self.analogs ) )
+
+        # Add or remove counter if counter is in input stream
+        if self.counter:
+            # User does not want to get counter
+            if self.counter_skip:
+                mask = [False] + mask
+            # User want the counter
+            else:
+                mask = [True] + mask
+
+        # Add or remove status if status is in input stream
+        if self.status:
+            mask = mask + [True]
 
         print( f"mask(2): {mask}")
 
@@ -243,37 +260,52 @@ class MemsArrayNP( MemsArray ):
         transfer_index = 0                                          
 
         try:
-            # Get chunk of data from input numpy array
-            log.info( f" .Reading input stream {self.__input.shape[0]} x {self.__input.shape[1]}" )
+            # Get first chunk of data from input callback
+            log.info( f" .Executing user callback function..." )
+
+            # Get first chunk of data from input callback
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            
+            self.__input = self.__user_in_callback( *self.__user_in_callback_args )
+            self.__input_dtype = self.__input.dtype
+
+            frame_length, channels_number = self.__input.shape
+            if frame_length != self.frame_length:
+                log.warning( f" .Input data frame length ({frame_length}) does not match the requested frame length ({self.frame_length})" )
+                log.info( f" .Change the frame length to {frame_length}" )
+                self.setFrameLength( frame_length )
+            
+            if channels_number != self.channels_number:
+                if not self.status and channels_number == self.channels_number + 1:
+                    log.warning( f" .Input data channels number ({channels_number}) does not match the requested channels number ({self.channels_number})" )
+                    log.info( f" .Add the status as available channel. Please verify that I am right !" )
+                    mask = mask + [False]
+                else:
+                    raise MuCBException( f"Input data channels number ({channels_number}) does not match the requested channels number ({self.channels_number})" )
+
+            log.info( f" .Input data shape: {self.__input.shape} ({self.frame_length} data x {self.channels_number})" )
 
             transfert_start_time = time()
             frame_duration = self.frame_length / self.sampling_frequency
             processing_delay = frame_duration * DB_PROCESSING_DELAY_RATE
             self.setRunningFlag( True )
             while self.running:
-                # Get chunk of data from input numpy array
-                
-                chunk = self.__input[ self.__input_index : self.__input_index + self.frame_length, : ]
-
                 # Process binary data by pushing them in the queue 
                 # Thanks to the queue, data are not lost if the reading process is too slow compared to the filling speed.
                 # However, the queue introduces a latency that can become problematic.
                 # If the user accepts the loss of data, it is possible to limit the size of the queue.
                 # In this case, once the size is reached, each new entry induces the deletion of the oldest one.
 
-                # Select channels
+                # No more data to process -> end of loop
+                if self.__input.shape[0] != self.frame_length:
+                    self.setRunningFlag( False )
+                    break
+
+                # Get chunk of data from input numpy array and select channels
                 if masking:
-                    chunk = chunk[mask,:]
-
-                # Add counter values
-                if self.counter and not self.counter_skip:
-                    counter_values = np.arange( self.__input_index, self.__input_index + self.frame_length )
-                    chunk = np.insert( chunk, 0, counter_values, axis=1 ),
-
-                # Add 0 as status values 
-                if self.status:
-                    status_values = np.zeros( ( self.frame_length, 1 ) )
-                    chunk = np.append( chunk, status_values, axis=1 )
+                    chunk = self.__input[:,mask]
+                else:
+                    chunk = self.__input
 
                 # Push data in the queue after conversion
                 self.queue.put( self.__run_process_data( chunk ) )
@@ -286,15 +318,16 @@ class MemsArrayNP( MemsArray ):
                     self.setRunningFlag( False )
 
                 # Wait for the next transfert
-                if ( time() - transfert_start_time ) < frame_duration - processing_delay:
-                    sleep( frame_duration-time()+transfert_start_time-processing_delay )
+                #if ( time() - transfert_start_time ) < frame_duration - processing_delay:
+                #    sleep( frame_duration-time()+transfert_start_time-processing_delay )
 
                 # Next transfert start time
                 transfert_start_time = time()
+                self.__input = self.__user_in_callback( *self.__user_in_callback_args )
 
             log.info( " .Running stopped: normal thread termination" )
 
-        except MuNPException as e:
+        except MuCBException as e:
             # Known exception:
             log.info( f" .Listening loop was stopped: {e}" )
         except Exception as e:
