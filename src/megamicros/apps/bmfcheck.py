37cb51ff8d7e05ca1419f4634c1009af0dc60583
Bruno Gas <bruno.gas@bimea.io>
2024-06-17 01:47:24 +0200

    update

--- src/megamicros/apps/bmfcheck.py
+++ src/megamicros/apps/bmfcheck.py
@@ -49,7 +49,7 @@ from megamicros import __version__
 from megamicros.apps import welcome_msg
 
 DEFAULT_MQTT_HOST = 'mqtt.bimea.tech'
-DEFAULT_MQTT_SUB_TOPIC = 'romille/mater/1/device/mu32/poc2-2/status'
+DEFAULT_MQTT_SUB_TOPIC = 'romille/mater/1/device/mu32/poc2-1/status'
 
 def arg_parse() -> dict:
 
@@ -91,60 +91,176 @@ def on_message( client, userdata, msg):
     else:
         return
     
-    BFE = np.array( report['BFE'] )
-    mems_position =np.array( report['mems_position'] )
-    locations_position = np.array( report['locations_position'] )
-    sampling_frequency = report['sampling_frequency']
-    frame_length = report['frame_length']
-    max_energy_index = report['max_energy_index']
+    if report['app'] == 'bmfcheck':
 
-    # print area locations and antenna 
-    fig = plt.figure()
-    ax = fig.add_subplot( 131, projection='3d' )
-    ax.scatter( mems_position[:,0], mems_position[:,1], mems_position[:,2] )
-    ax.scatter( locations_position[:,0], locations_position[:,1], locations_position[:,2] )
-    ax = fig.add_subplot( 132, projection='3d' )
+        BFE = np.array( report['BFE'] )
+        mems_position =np.array( report['mems_position'] )
+        locations_position = np.array( report['locations_position'] )
+        n_x = report['n_width']
+        n_y = report['n_depth']
+        sampling_frequency = report['sampling_frequency']
+        frame_length = report['frame_length']
+        max_energy_index = report['max_energy_index']
 
-    ax.set_xlabel( 'X' )
-    ax.set_ylabel( 'Y' )
-    ax.scatter( mems_position[:,0], mems_position[:,1], mems_position[:,2], c=np.arange(32) )
-    fig.show()
-    car = input( 'Press any key to continue' )
+        # print area locations and antenna 
+        fig = plt.figure()
+        ax = fig.add_subplot( 131, projection='3d' )
+        ax.scatter( mems_position[:,0], mems_position[:,1], mems_position[:,2] )
+        ax.scatter( locations_position[:,0], locations_position[:,1], locations_position[:,2] )
+        ax = fig.add_subplot( 132, projection='3d' )
 
-    plt.figure()
-    BFE = np.reshape( BFE, (50, 50) )
-    plt.xlabel('width')
-    plt.ylabel('height')
-    plt.imshow( BFE, origin='lower', aspect='equal')
-    plt.show()
-    print(BFE)
-    car = input( 'Press any key to continue' )
+        ax.set_xlabel( 'X' )
+        ax.set_ylabel( 'Y' )
+        ax.scatter( mems_position[:,0], mems_position[:,1], mems_position[:,2], c=np.arange(32) )
+        fig.show()
+        car = input( 'Press any key to continue' )
 
+        plt.figure()
+        BFE = np.reshape( BFE, (n_x, n_y) )
+        plt.xlabel('width')
+        plt.ylabel('height')
+        plt.imshow( BFE, origin='lower', aspect='equal')
+        plt.show()
+        print(BFE)
+        car = input( 'Press any key to continue' )
+
+    elif report['app'] == 'bmfsim':
+
+        BFE = np.array( report['BFE'] )
+        BFE_index = report['BFE_index']
+        mems_position =np.array( report['mems_position'] )
+        locations_position = np.array( report['locations_position'] )
+        n_x = report['n_width']
+        n_y = report['n_depth']
+        sampling_frequency = report['sampling_frequency']
+        frame_length = report['frame_length']
+        channel_1_signal = np.array( report['channel_1_signal'] )
+        channel_2_signal = np.array( report['channel_2_signal'] )
+        source_position = np.array( report['source_position'] )
+        fftchannels = np.array( report['fftchannels'] )
+
+        # show antenna and locations
+        fig = plt.figure()
+        ax = fig.add_subplot( 131, projection='3d' )
+
+        # print antenna in area locations
+        ax.scatter( mems_position[:,0], mems_position[:,1], mems_position[:,2] )
+        ax.scatter( locations_position[:,0], locations_position[:,1], locations_position[:,2] )
+        ax = fig.add_subplot( 132, projection='3d' )
+        ax.set_xlabel( 'X' )
+        ax.set_ylabel( 'Y' )
+        ax.scatter( mems_position[:,0], mems_position[:,1], mems_position[:,2], c=np.arange(32) )
+        fig.show()
+        #input( 'Press any key to continue' )
+
+        # print BFE indexes
+        fig2 = plt.figure()
+        plt.xlabel('location index')
+        plt.ylabel('prediction index')
+        plt.plot( BFE_index )
+        fig2.show()
+        #input( 'Press any key to continue' )
+
+        # print signals
+        t = np.array( [i for i in range( frame_length )] ) * 1000 / sampling_frequency
+        fig3 = plt.figure(3)
+        plt.plot( t, channel_1_signal, t, channel_2_signal )
+        fig3.show()
+        #input( 'Press any key to continue' )
+
+        fig4 = plt.figure()
+        #for i in range( len( BFE ) ):
+        #    BFEdisplay = np.reshape( np.array( BFE[i] ), (n_x, n_y) )
+        #    plt.imshow( BFEdisplay, origin='lower', aspect='equal')
+        #    plt.draw()  # Mettez à jour la figure
+        #    plt.pause(0.001)  # Pause nécessaire pour permettre la mise à jour de la figure
+        #    input('Press any key to continue')
+        #    plt.clf()  # Effacez la figure pour le prochain affichage
+
+
+        BFEshow = np.reshape( BFE, (n_x, n_y) )
+        plt.xlabel('width')
+        plt.ylabel('height')
+        plt.imshow( BFEshow, origin='lower', aspect='equal')
+        #plt.pcolormesh( BFEshow )
+        plt.draw()  # Mettez à jour la figure
+        plt.pause(0.001)  # Pause nécessaire pour permettre la mise à jour de la figure
+        #print(BFEshow)
+        #input( 'Press any key to continue' )
+
+        max_index_BFE = np.argmax( BFE )
+        max_index_BFE_location = locations_position[max_index_BFE,:]
+        min_index_BFE = np.argmin( BFE )
+        min_index_BFE_location = locations_position[min_index_BFE,:]
+
+        fig5 = plt.figure()
+
+        plt.scatter( locations_position[:,0], locations_position[:,1] )
+        plt.scatter( mems_position[:24,0], mems_position[:24,1], c=np.arange(24) )
+        plt.scatter( source_position[0], source_position[1], c='r' )
+        plt.scatter( max_index_BFE_location[0], max_index_BFE_location[1], c='g' )
+        plt.scatter( min_index_BFE_location[0], min_index_BFE_location[1], c='y' )
+        plt.xlabel('width')
+        plt.ylabel('height')
+        fig5.show()
+
+        print( 'max PFE=', max_index_BFE_location, ', value=', BFE[max_index_BFE] )
+        print( 'min PFE=', min_index_BFE_location, ', value=', BFE[min_index_BFE])
+        print( 'BFEshow=', BFEshow)
+
+        fig6 = plt.figure()
+        plt.title( 'BFE at x=n_x/2' )
+        plt.plot( BFEshow[int(n_x/2),:] )
+        fig6.show()
+
+        print( 'mems_position=', mems_position )
+
+        # Plot FFT module
+        fig7 = plt.figure()
+        channel_number = 5
+        fft_module = fftchannels[channel_number]
+        plt.title( 'FFT module on channels' )
+        n_f = len(fft_module)
+        f = np.array( [i for i in range( n_f )] ) * sampling_frequency / n_f / 2
+        plt.plot( f, np.array( fft_module ) )
+        fig7.show()
+
+        input( 'Press any key to continue' )
+
+        plt.close( 'all' )
 
 
 def main():
         
-        args = arg_parse()
-    
-        print( welcome_msg )
-    
-        # Connect to MQTT broker
-        client = mqtt.MqttClient( host=args['host'], name='bmfcheck' )
-        if not client.is_connected():
-            log.error( "Failed to connect to MQTT broker" )
-            exit(1)
-    
-        # Subscribe to MQTT topic
-        client.subscribe( args['topic'] )
-        log.info( f"Subscribed to topic {args['topic']}" )
+    args = arg_parse()
 
-        # run callback function
-        try:
-            client.run( args['topic'], on_message )
-        except KeyboardInterrupt:
-            print( "\nExiting..." )
-            exit(0)
+    print( welcome_msg )
+
+    data = np.load( 'P2.npy' )
+    print(data)
+    
+
+
+
+
+    # Connect to MQTT broker
+    client = mqtt.MqttClient( host=args['host'], name='bmfcheck' )
+    if not client.is_connected():
+        log.error( "Failed to connect to MQTT broker" )
+        exit(1)
+
+    # Subscribe to MQTT topic
+    client.subscribe( args['topic'] )
+    log.info( f"Subscribed to topic {args['topic']}" )
+
+    # run callback function
+    try:
+        client.run( args['topic'], on_message )
+    except KeyboardInterrupt:
+        print( "\nExiting..." )
+        exit(0)
 
 
 if __name__ == "__main__":
     main()
+
