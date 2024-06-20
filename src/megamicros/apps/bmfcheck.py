ee619ba9dd8315348241b57031498f93fa9c4d08
Bruno Gas <bruno.gas@biimea.com>
2024-06-20 19:59:27 +0200

    update

--- src/megamicros/apps/bmfcheck.py
+++ src/megamicros/apps/bmfcheck.py
@@ -169,13 +169,13 @@ def on_message( client, userdata, msg):
         plt.imshow( BFE, origin='lower', aspect='equal')
         plt.show()
         print(BFE)
-        car = input( 'Press any key to continue' )
+        input( 'Press any key to continue' )
 
     elif report['app'] == 'bmfsim':
 
         # Load P1-2 data
-        data = np.load( 'P2.npy' )
-        print( 'P2=', data)
+        # data = np.load( 'P2.npy' )
+        # print( 'P2=', data)
 
         # Get data from server
         BFE = np.array( report['BFE'] )
@@ -191,7 +191,6 @@ def on_message( client, userdata, msg):
         frame_length = report['frame_length']
         source_position = np.array( report['source_position'] )
         source_frequency = np.array( report['source_frequency'] )
-        D = np.array( report['D'] )
 
         # show antenna and locations
         fig = plt.figure()
@@ -239,14 +238,63 @@ def on_message( client, userdata, msg):
         ax.invert_yaxis()
         fig2.show()
 
-        # Print matrix distances
-        #fig3 = plt.figure()
-        #print( "D=", D)
-        #print( "D.shape=", np.shape(D) )
-        #print( "Distance min: ", np.min( D ) )
-        #print( "Distance max: ", np.max( D ) )
-        #plt.pcolormesh( D )
-        #fig3.show()
+        input( 'Press any key to continue' )
+
+    elif report['app'] == 'h5bmfrun':
+
+        # Get data from server
+        BFE = np.array( report['BFE'] )
+        max_energy_index = report['max_energy_index']
+        mems_number = report['mems_number']
+        mems_position =np.array( report['mems_position'] )
+        locations_position = np.array( report['locations_position'] )
+        width = report['width']
+        depth = report['depth']
+        n_x = report['n_width']
+        n_y = report['n_depth']
+        sampling_frequency = report['sampling_frequency']
+        frame_length = report['frame_length']
+
+        # show antenna and locations
+        fig = plt.figure()
+
+        # print antenna
+        ax = fig.add_subplot( 121, projection='3d' )
+        ax.set_xlabel( 'Width' )
+        ax.set_ylabel( 'Depth' )
+        ax.scatter( mems_position[:,0], mems_position[:,1], mems_position[:,2], c=np.arange(32) )
+
+        # print locations
+        ax = fig.add_subplot( 122, projection='3d' )
+        ax.set_aspect('equal')
+        ax.scatter( mems_position[:,0], mems_position[:,1], mems_position[:,2], c=np.arange(32) )
+        ax.scatter( locations_position[:,0], locations_position[:,1], locations_position[:,2], 'ob' )
+        ax.set_xlabel( 'Width' )
+        ax.set_ylabel( 'Depth' )
+        fig.show()
+
+        # Print BFE
+        fig2 = plt.figure()
+        ax = fig2.add_subplot(111)
+        BFEimg = np.reshape(BFE, (n_y, n_x) )
+        BFE_max_index = np.argmax( BFE )
+        BFE_max_position = locations_position[BFE_max_index,:]
+        XM = BFE_max_position[0]
+        YM = BFE_max_position[1]
+        print( 'BFE_max_index=', BFE_max_index)
+        print( 'BFE_max_position=', BFE_max_position)
+        xs = np.arange( -width/2, +width/2, width/n_x )
+        ys = np.arange( -depth/2, +depth/2, depth/n_y )
+        ax.imshow( BFEimg, extent=[ xs[0], xs[-1], ys[-1], ys[0] ], cmap='Greys_r', origin='upper')
+        ax.contourf( BFEimg, 20, extent=[ xs[0], xs[-1], ys[-1], ys[0] ], cmap='Greys_r', origin='upper' )
+        ax.contour( BFEimg, 20, extent=[ xs[0], xs[-1], ys[-1], ys[0] ], origin='upper' )
+        ax.scatter( XM, YM, c='r' )
+        ax.grid( True )
+        ax.set_aspect('equal')
+        ax.set_xlabel( 'x [m]' )
+        ax.set_ylabel( 'y [m]' )
+        ax.invert_yaxis()
+        fig2.show()
 
         input( 'Press any key to continue' )
 
